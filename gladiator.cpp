4ed74d36ff7e3b3c3031cf242f4661d0cb871c4e
Gustavo Segura <gsegura96@openmailbox.org>
2017-05-09 23:06:18 -0600

    gitignore

--- gladiator.cpp
+++ gladiator.cpp
@@ -4,3 +4,18 @@ Gladiator::Gladiator()
 {
 
 }
+
+float Gladiator::getSpeed() const
+{
+    return speed;
+}
+
+void Gladiator::setSpeed(float value)
+{
+    speed = value;
+}
+
+
+namespace BattleUtils {
+
+}
