a11aa311985b5d1c521cc15774ecb54ea0ebe3d3
jimena <jimenamaria598@gmail.com>
2017-05-19 00:24:21 -0600

    mov arreglado

--- gladiatorManager.cpp
+++ gladiatorManager.cpp
@@ -50,50 +50,6 @@ int gladiatorManager::run(sf::RenderWindow &window) {
                 return 0;
         }
 
-        for (int gen = 0; gen < 1; ++gen) {
-            resultPop.clear();
-            gladiatorList.clear();
-
-            for (int i = 0; i < population.population.size(); ++i) {
-                gladiator.setDna(population.getPopulation()[i]);
-                gladiator.setTexture(&Gtexture);
-                gladiator.sprite.setScale(0.3,0.3);
-                gladiator.setPosition(sf::Vector2f(300,300));
-                gladiator.setLabyrinthDirections(labyrinthDirections);
-                gladiatorList.push_back(gladiator);
-            }
-
-
-            while (cycleClock.getElapsedTime().asMilliseconds() < 4000/Xspeed) {
-                window.draw(bg1Sprite);
-                for (int i = 0; i < gladiatorList.size(); ++i) {
-                    gladiatorList[i].update();
-                    gladiatorList[i].draw(window);
-                }
-                window.display();
-
-
-                while (window.pollEvent(event))
-                {
-                    if (event.type == sf::Event::Closed)
-                        exit(0);
-                    if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
-                        return 0;
-                }
-
-            }
-            cycleClock.restart();
-            for (int i = 0; i < gladiatorList.size(); ++i) {
-                gladiatorList[i].testCalcFitness(sf::Vector2f(1000, 600));
-                resultPop.push_back(gladiatorList[i].dna);
-            }
-            population.setPopulation(resultPop);
-            population.sortByFitness();
-            std::cout<<"gen  "<<gen <<"best "<<population.population[0].getFitness()<<"\n";
-            population.createNextGeneration();
-        }
-        break;
-
     }
 
 
